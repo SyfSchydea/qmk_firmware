86e4611127b46da5f174dda6e390a0ff6a69b1ff
SyfSchydea <sashapwn@gmail.com>
2024-09-06 12:10:09 +0100

    Change LED code so only some layers have LED settings

--- keyboards/ergodox_ez/keymaps/SyfSchydea/keymap.c
+++ keyboards/ergodox_ez/keymaps/SyfSchydea/keymap.c
@@ -2,7 +2,7 @@
 #include "version.h"
 #include "keymap_uk.h"
 
-enum layers {
+enum layer {
 	BASE,  // default layer
 	STNO,  // Steno Layer
 
@@ -277,70 +277,67 @@ void keyboard_post_init_user(void) {
 #endif
 };
 
+#define LED_BLACK() do{ergodox_right_led_1_off();ergodox_right_led_2_off();ergodox_right_led_3_off();}while(0)
+#define LED_RED()   do{ergodox_right_led_1_on(); ergodox_right_led_2_off();ergodox_right_led_3_off();}while(0)
+#define LED_WHITE() do{ergodox_right_led_1_on(); ergodox_right_led_2_on(); ergodox_right_led_3_on(); }while(0)
+
 // Runs whenever there is a layer state change.
 layer_state_t layer_state_set_user(layer_state_t state) {
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
+	ergodox_board_led_off();
 
-    uint8_t layer = get_highest_layer(state);
-    switch (layer) {
-        case BASE:
+	if (layer_state_cmp(state, SPEC)) {
+		LED_WHITE();
+	} else if (layer_state_cmp(state, STNO)) {
+		LED_RED();
+	} else { // BASE
+		LED_BLACK();
+	}
+
+	uint8_t layer = get_highest_layer(state);
+	switch (layer) {
+		case 0:
 #ifdef RGBLIGHT_COLOR_LAYER_0
-            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
 #endif
-            break;
-        case STNO:
-            ergodox_right_led_1_on();
+			break;
+		case 1:
 #ifdef RGBLIGHT_COLOR_LAYER_1
-            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
 #endif
-            break;
-        // case 2:
-            // ergodox_right_led_2_on();
-// #ifdef RGBLIGHT_COLOR_LAYER_2
-            // rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
-// #endif
-            // break;
-        // case 3:
-            // ergodox_right_led_3_on();
-// #ifdef RGBLIGHT_COLOR_LAYER_3
-            // rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
-// #endif
-            // break;
-        case SYMB:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
+			break;
+		case 2:
+#ifdef RGBLIGHT_COLOR_LAYER_2
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+#endif
+			break;
+		case 3:
+#ifdef RGBLIGHT_COLOR_LAYER_3
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+#endif
+			break;
+		case 4:
 #ifdef RGBLIGHT_COLOR_LAYER_4
-            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
 #endif
-            break;
-        case MDIA:
-            ergodox_right_led_1_on();
-            ergodox_right_led_3_on();
+			break;
+		case 5:
 #ifdef RGBLIGHT_COLOR_LAYER_5
-            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
 #endif
-            break;
-        // case 6:
-            // ergodox_right_led_2_on();
-            // ergodox_right_led_3_on();
-// #ifdef RGBLIGHT_COLOR_LAYER_6
-            // rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-// #endif
-            // break;
-        case SPEC:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
-            ergodox_right_led_3_on();
+			break;
+		case 6:
+#ifdef RGBLIGHT_COLOR_LAYER_6
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+#endif
+			break;
+		case 7:
 #ifdef RGBLIGHT_COLOR_LAYER_7
-            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+			rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
 #endif
-            break;
-        default:
-            break;
-    }
+			break;
+		default:
+			break;
+	}
 
-    return state;
+	return state;
 };
